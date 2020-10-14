91b883dfaabc58ac20460fb787e4eebc5914617f
vishal mistry <54712564+vishalmistry499@users.noreply.github.com>
2020-10-14 15:50:22 +0530

    adding the lane spinner

--- app/src/main/java/com/example/ultrafficbot/MainActivity.java
+++ app/src/main/java/com/example/ultrafficbot/MainActivity.java
@@ -1,21 +1,43 @@
 package com.example.ultrafficbot;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.RelativeLayout;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbar = findViewById(R.id.materialToolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_menu_24px);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
 
     }
+
+
 
     public void sendToManual(View view) {
         Intent intent = new Intent(MainActivity.this, Manual.class);
@@ -23,10 +45,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void sendToAutomatic(View view) {
-        Intent intent = new Intent(MainActivity.this,AutomaticMode.class);
+        Intent intent = new Intent(MainActivity.this, AutomaticMode.class);
         startActivity(intent);
     }
-
-
-
 }
